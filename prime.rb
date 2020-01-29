43c28c826a7e14c83770edf07bd5f5a9ef15a004
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-29 15:46:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(i)
   n = 2
   x = i.abs
   while n < 10 
-    if x % n == 0 
-      return false 
+    if (x % n =/= 0) || x == 2 || x == 3 || x == 5 || x == 7
+      return false  
     else
       return true 
     end
