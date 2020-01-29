1d2c01dfd9c07e3936b2cdfbd0032ce297b2d7df
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-29 15:42:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,12 @@
-
-  n = 2
+def prime?(i)
+  y = 2
   x = i.abs
   while n < 10 
     if x % n == 0 
-      false 
+      return false 
     else
-      true 
+      return true 
     end
   n += 1
   end
-end
-prime_array = []    
-p = 2
-if n < 2
-    return p
-end
-
-while (n % p == 0) && (p < n)
-    prime_array.push(p)
-    p += 1
 end
